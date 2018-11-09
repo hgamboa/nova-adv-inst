7555aa7a2328b6afead0a010d6868f56da127f9e
Daniel Osório <vdosavh@gmail.com>
2018-11-09 13:42:27 +0000

    Refracted server

--- lab9/server.py
+++ lab9/server.py
@@ -9,32 +9,31 @@ import random
 import time
 
 # This class will be called when you visit or make a GET request
-# to http:RASPIP:9999/ and will return the example.html page inside
+# to http:RASPIP:8888/ and will return the example.html page inside
 # the static folder.
 class index(tornado.web.RequestHandler):
-	def get(self):
-		self.render("static/example.html")
-
+    def get(self):
+        self.render("static/example.html")
 # This class is responsible to create a websocket between the client
 # and the server, allowing for a persistent connection between them 
 # and the hability of sending data in a fast and reliable way.
 # To check the connection flow consult lab 9 readme.md file.
 class WSHandler(tornado.websocket.WebSocketHandler):
-	# This method will be called when a new client connects to the websocket
-	def open(self):
-		print('New Connection')
-	
-	# This method will be called when a new message from a client arrive
-	def on_message(self, message):
-		# Messages in websockets are usually passed as a JSON message (In string form).
-		# JSON is more a less a dictionary, where you can have multiple
-		# of key:values pairs.
-		# The following line parses these json messages to python dictionaries
-		js = tornado.escape.json_decode(message)
+    # This method will be called when a new client connects to the websocket
+    def open(self):
+        print('New Connection')
+    
+    # This method will be called when a new message from a client arrive
+    def on_message(self, message):
+        # Messages in websockets are usually passed as a JSON message (In string form).
+        # JSON is more a less a dictionary, where you can have multiple
+        # of key:values pairs.
+        # The following line parses these json messages to python dictionaries
+        js = tornado.escape.json_decode(message)
 
-		# This 'type' is define in our html page. 
-		if(js['type'] == 'sendData'):
-			self.write_message({'timestamp': time.time(), 'data': random.randint(0,10)})
+        # This 'type' is define in our html page. 
+        if(js['type'] == 'sendData'):
+            self.write_message({'timestamp': time.time(), 'data': random.randint(0,10)})
 
 # We need to tell our webserver what is the path to the static folder, since the
 # html page will need to access files from this folder.
@@ -45,8 +44,8 @@ settings = {"static_path": os.path.join(os.path.dirname(__file__), "static")}
 # r'/ws' is connected to web socket handler. This is called routing.
 
 application = tornado.web.Application(handlers=[
-	(r'/', index),
-	(r'/ws', WSHandler)
+    (r'/', index),
+    (r'/ws', WSHandler)
 ], **settings)
 
 # This will launch our http server on port 9999. To access it
